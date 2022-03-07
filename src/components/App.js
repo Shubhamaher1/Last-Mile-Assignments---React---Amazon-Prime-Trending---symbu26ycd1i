57465d1c2fc7f07c8e20beff4177db3f35da2759
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-03-07 19:34:37 +0530

    ja

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import slides from "../data";
 
 const App = () => {
+  const [fist, setfirst] = useState(0);
+  function find() {
+    if (fist === 0) {
+      return (
+        <>
+          <button data-testid="button-restart" disabled>
+            Restart
+          </button>
+          <button data-testid="button-prev" disabled>
+            prev
+          </button>
+          <button data-testid="button-next">Next</button>
+        </>
+      );
+    } else if (fist === 4) {
+      return (
+        <>
+          <button data-testid="button-restart">Restart</button>
+          <button data-testid="button-prev">prev</button>
+          <button data-testid="button-next" disabled>
+            Next
+          </button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button data-testid="button-restart">Restart</button>
+          <button data-testid="button-prev">prev</button>
+          <button data-testid="button-next">Next</button>
+        </>
+      );
+    }
+  }
   return (
-    <></>
-  )
-}
-
+    <div>
+      <h1 data-testid="title"></h1>
+      <p data-testid="text"></p>
+      {}
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import slides from './data.js';
+import slides from "./data.js";
 
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+ReactDOM.render(<App slides={slides} />, document.getElementById("root"));
