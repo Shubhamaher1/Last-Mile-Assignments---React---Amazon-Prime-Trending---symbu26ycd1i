30e6d28c86a54e38839b7d02ef3a03112a816387
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-03-11 16:34:30 +0530

    as

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,80 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import slides from "../data";
 
-const App = () => {
-  const [fist, setfirst] = useState(0);
-  function find() {
-    if (fist === 0) {
-      return (
-        <>
-          <button data-testid="button-restart" disabled>
-            Restart
-          </button>
-          <button data-testid="button-prev" disabled>
-            prev
-          </button>
-          <button data-testid="button-next">Next</button>
-        </>
-      );
-    } else if (fist === 4) {
-      return (
-        <>
-          <button data-testid="button-restart">Restart</button>
-          <button data-testid="button-prev">prev</button>
-          <button data-testid="button-next" disabled>
-            Next
-          </button>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <button data-testid="button-restart">Restart</button>
-          <button data-testid="button-prev">prev</button>
-          <button data-testid="button-next">Next</button>
-        </>
-      );
-    }
-  }
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  const [data, setdata] = useState(props.slides[2]);
+
   return (
-    <div>
-      <h1 data-testid="title"></h1>
-      <p data-testid="text"></p>
-      {}
-    </div>
+    <>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
+      {index === 0 ? (
+        <button
+          data-testid="button-prev"
+          disabled={true}
+          // style={{ display: "none", }}
+          onClick={() => {
+            setIndex(index - 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Prev
+        </button>
+      ) : (
+        <button
+          data-testid="button-prev"
+          onClick={() => {
+            setIndex(index - 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Prev
+        </button>
+      )}
+      {index === 4 ? (
+        <button
+          disabled={true}
+          data-testid="button-next"
+          // style={{ display: "none" }}
+          onClick={() => {
+            setIndex(index + 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Next
+        </button>
+      ) : (
+        <button
+          data-testid="button-next"
+          onClick={() => {
+            setIndex(index + 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Next
+        </button>
+      )}
+      {index === 0 ? (
+        <button
+          data-testid="button-restart"
+          //  style={{ display: "none" }}
+          disabled={true}
+        >
+          Restart
+        </button>
+      ) : (
+        <button
+          data-testid="button-restart"
+          onClick={() => {
+            setIndex(0);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Restart
+        </button>
+      )}
+    </>
   );
 };
 
